c9e5334951a0f5377b3938eb24ed54df47a26fde
Yordan Neshev <dankoneshev@abv.bg>
2025-12-17 19:47:35 +0200

    initial, reworked ascii screen, started work on stat bars

--- BarsAndRoaches.cpp
+++ BarsAndRoaches.cpp
@@ -1,6 +1,155 @@
+#include <fstream>
+#include <iostream>
+#include <windows.h> // needed for console encoding switch
+
+/*
+struct student
+{
+    double money;
+    int energy;
+    int psyche;
+    int knowledge;
+    int passed_exams;
+};
+*/
+
+const int INPUT_LINE_MAX_SIZE = 100;
+const int SEMESTER_LENGTH = 45;
+const char BASE_FILE_NAME[] = "SaveSlots.txt";
+
+void writeInFile(const char* fileName, const char* content)
+{
+    std::ofstream outputStream (fileName, std::ios_base::app);
+
+    if (!outputStream.is_open())
+    {
+        std::cout << "Файлът не се записа успешно!" << std::endl;
+        return;
+    }
+
+    outputStream << content << std::endl;
+    
+    outputStream.close();
+}
+
+bool readFile(const char* fileName)
+{
+    std::ifstream inputStream (fileName);
+
+    if (!inputStream.is_open())
+    {
+        std::cout << "Файлът не се прочете успешно!" << std::endl;
+        return false;
+    }
+
+    std::cout << inputStream.rdbuf(); // print all lines (USE ANOTHER WAY), index them so a player can choose one of them
+    
+    inputStream.close();
+    return true;
+}
+
+void calculateStatBar()
+{
+    
+}
 
 int main(int argc, char* argv[])
 {
+    SetConsoleOutputCP(CP_UTF8); // switch to utf-8 encoding, so the console recognizes the ascii symbols
     
-    return 0;
+    // alternative title: Cock(tails + roaches)
+
+    int commandLine = 0;
+    
+        // save-file load system
+    std::cout << "╭──────────────────────────╮ \n"
+              << "│     Bars and Roaches     │ \n"
+              << "│       [1] Нов файл       │ \n"
+              << "│       [2] Продължи       │ \n"
+              << "╰──────────────────────────╯" << std::endl;
+
+    do
+    {
+        std::cin >> commandLine;
+
+        char saveFileName[INPUT_LINE_MAX_SIZE];
+        
+        if (commandLine == 1)
+        {
+            std::cout << "Как искаш да кръстиш новия записан файл?" << '\n';
+            std::cin >> saveFileName;
+
+            writeInFile(BASE_FILE_NAME, saveFileName);
+            
+            break;
+        }
+        else if (commandLine == 2)
+        {
+            std::cout << "От кой записан файл искаш да продължиш?" << '\n';
+
+            if (!readFile(BASE_FILE_NAME))
+                continue;
+            
+            // make a way to choose from printed files
+            break;
+        }
+        else
+        {
+            std::cout << "Невалидна команда, опитай отново!" << '\n';
+        }
+    }
+    while (true);
+
+	bool isNewGame = commandLine % 2; // true or false depending on if saveFile is new
+    
+    // student mainCharacter;   initialization of main character
+
+    for (int day = 1; day <= SEMESTER_LENGTH; day++)
+    {
+        std::cout << "╭───────────────────────────╮\n"
+                  << "     Ден " << day << " от " <<  SEMESTER_LENGTH << "\n"
+                  << " ─────────────────────────── \n"
+                  << "     » Пари: " << 999999 << "\n"
+                  << "     » Енергия: " << 100 << "\n"
+                  << "     » Психика: " << 100 << "\n"
+                  << "     » Здраве:  " << 100 << "\n"
+                  << "     » Знания:  " << 100 << "\n"
+                  << "     » Взети изпити:  " << 9 << "\n"
+                  << "╰───────────────────────────╯" << std::endl; // weird, but all are it's centered
+        
+        std::cout << "Какво искаш да направиш днес? {Ден: " << day << "}" << '\n'
+            << "[1] Учиш \n"
+            << "[2] Храниш се \n"
+            << "[3] Излизаш \n"
+            << "[4] Почиваш \n"
+            << "[5] Работиш \n"
+            << "[6] Явяваш се на изпит (ако е време) \n" // only show if its exam day
+            << "[11] Излез от играта" << std::endl;
+
+        // more actions, make some dynamic way to output them
+        
+        std::cin >> commandLine;
+
+        if (commandLine == 1)
+        {
+            // u study
+        }
+            
+        else if (commandLine == 2)
+        {
+            // u eat
+        } 
+        
+        // more cases for each action
+
+        else if (commandLine == 11)
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Невалидно действие, опитай отново!";
+            day--;
+        }
+    }
 }
